e44e993f0d85b1eed64612f63b2ff7813924a10f
shadow <lightshadow416@gmail.com>
2023-09-04 09:50:45 +0100

    making flex layout in container component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -4,8 +4,8 @@ import { Input } from "../../components";
 
 export default function Container() {
   return (
-    <>
+    <div className="border-2 border-red-500 h-screen flex justify-center">
       <Input />
-    </>
+    </div>
   );
 }
